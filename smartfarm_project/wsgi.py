017f7eb7a97040478a29dcc59b2242e198bd9f67
Amar Singh <singhyadavamar425@gmail.com>
2026-07-23 21:30:08 +0530

    Optimize WSGI for Render deployment and hide TF logs

--- smartfarm_project/wsgi.py
+++ smartfarm_project/wsgi.py
@@ -9,6 +9,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+# 1. TensorFlow Warnings & Logs ko hide karne ke liye
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+# 2. Render Free Tier par Memory Full / Freeze hone se bachane ke liye (CPU Optimization)
+os.environ['TF_NUM_INTEROP_THREADS'] = '1'
+os.environ['TF_NUM_INTRAOP_THREADS'] = '1'
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartfarm_project.settings')
