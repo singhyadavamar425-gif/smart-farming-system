99a48206bebc633c50c9221d95625335a3acbac3
Amar Singh <singhyadavamar425@gmail.com>
2026-07-23 11:36:54 +0530

    Updated pest page with PWA meta tags and full code

---
+++ dashboard/static/serviceworker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'smartfarm-v1';
+const urlsToCache = [
+    '/',
+    '/static/pest_detection/css/style.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
